e1919a3a986e29974c0c696bf426123e65cca02a
abbyenriquez <abi.enriquez0119@gmail.com>
2026-04-09 16:33:07 -0700

    c1: add interactive greeting program

---
+++ week1/hello_student.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    string answer;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Hello, " << name << "! Welcome to C++!" << endl;
+
+    cout << "Please enter your favorite food: ";
+    cin >> answer;
+
+    cout << name << "! Wow, I love " << answer << " too (sometimes...)" << endl;
+
+    return 0;
+}
